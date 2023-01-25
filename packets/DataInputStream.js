8f8115daa4c3065b4f3f578c9afc0e6f97722567
mega12345mega <55769873+mega12345mega@users.noreply.github.com>
2023-01-25 11:38:17 -0800

    Add files via upload

--- packets/DataInputStream.js
+++ packets/DataInputStream.js
@@ -1,3 +1,10 @@
+/**
+ * This is a JavaScript translation of Java's DataInputStream
+ * Java source from https://developer.classpath.org/doc/java/io/DataInputStream-source.html
+ * Java's source, and hence this source, is licensed under GPL2
+ * The classpath exception still applies
+ * Refer to DataIOLicense.txt for a copy of GPL2
+ */
 class DataInputStream {
     constructor(bytes) {
         this.bytes = bytes;
@@ -5,32 +12,72 @@ class DataInputStream {
     read() {
         return this.bytes.shift();
     }
+    readBuffer(buf, offset, len) {
+        for (var i = 0; i < len; i++)
+            buf[offset + i] = this.read();
+    }
     readBoolean() {
         var byte = this.read();
         if (byte === 1) return true;
         else if (byte === 0) return false;
-        else throw new Error("Invalid boolean format! Expected a 1 or 0!");
+        else throw new Error("Invalid boolean format! Expected a 1 or 0! (Received " + byte + ")");
     }
     readByte() {
         return this.read();
     }
     readShort() {
-        return (this.read() << 8) + (this.read());
+        return (this.read() << 8) | (this.read());
     }
     readChar() {
         return String.fromCharCode(this.readShort());
     }
     readInt() {
-        return (this.read() << 24) + (this.read() << 16) + (this.read() << 8) + (this.read());
+        return (this.read() << 24) | (this.read() << 16) | (this.read() << 8) | (this.read());
     }
     readLong() {
-        return (this.read() << 56) + (this.read() << 48) + (this.read() << 40) + (this.read() << 32) + (this.read() << 24) + (this.read() << 16) + (this.read() << 8) + (this.read());
+        var view = new DataView(new ArrayBuffer(8));
+        for (var i = 0; i < 8; i++)
+            view.setUint8(i, this.read());
+        return view.getBigInt64(0);
     }
     readFloat() {
-        return this.intToFloat(this.readInt());
+        var view = new DataView(new ArrayBuffer(4));
+        for (var i = 0; i < 4; i++)
+            view.setUint8(i, this.read());
+        return view.getFloat32(0);
     }
     readDouble() {
-        return this.longToDouble(this.readLong());
+        var view = new DataView(new ArrayBuffer(8));
+        for (var i = 0; i < 8; i++)
+            view.setUint8(i, this.read());
+        return view.getFloat64(0);
+    }
+    readUTF() {
+        var UTFlen = this.readShort();
+        var buf = [];
+        this.readBuffer(buf, 0, UTFlen);
+        return this.convertFromUTF(buf);
+    }
+    convertFromUTF(buf) {
+        var strbuf = "";
+        
+        for (var i = 0; i < buf.length; ) {
+            if ((buf [i] & 0x80) === 0)        // bit pattern 0xxxxxxx
+                strbuf += (String.fromCharCode(buf [i++] & 0xFF));
+            else if ((buf [i] & 0xE0) === 0xC0) {  // bit pattern 110xxxxx
+                if (i + 1 >= buf.length || (buf [i + 1] & 0xC0) !== 0x80)
+                    throw new Error("UTFDataFormatException");
+                
+                strbuf += (String.fromCharCode(((buf [i++] & 0x1F) << 6) | (buf [i++] & 0x3F)));
+            } else if ((buf [i] & 0xF0) === 0xE0) {  // bit pattern 1110xxxx
+                if (i + 2 >= buf.length || (buf [i + 1] & 0xC0) !== 0x80 || (buf [i + 2] & 0xC0) !== 0x80)
+                    throw new Error("UTFDataFormatException");
+                
+                strbuf += (String.fromCharCode(((buf [i++] & 0x0F) << 12) | ((buf [i++] & 0x3F) << 6) | (buf [i++] & 0x3F)));
+            } else // must be ((buf [i] & 0xF0) == 0xF0 || (buf [i] & 0xC0) == 0x80)
+                throw new Error("UTFDataFormatException");    // bit patterns 1111xxxx or 10xxxxxx
+        }
+        return strbuf;
     }
     remaining() {
         return this.bytes.length;
--- packets/DataOutputStream.js
+++ packets/DataOutputStream.js
@@ -1,9 +1,20 @@
+/**
+ * This is a JavaScript translation of Java's DataOutputStream
+ * Java source from https://developer.classpath.org/doc/java/io/DataOutputStream-source.html
+ * Java's source, and hence this source, is licensed under GPL2
+ * The classpath exception still applies
+ * Refer to DataIOLicense.txt for a copy of GPL2
+ */
 class DataOutputStream {
     constructor() {
         this.bytes = [];
     }
     write(value) {
         this.bytes.push(value);
+    }
+    writeBuffer(buf, offset, len) {
+        for (var i = 0; i < len; i++)
+            this.writeByte(buf[offset + i]);
     }
     writeBoolean(value) {
         this.write(value ? 1 : 0);
@@ -25,20 +36,22 @@ class DataOutputStream {
         this.writeByte(value);
     }
     writeLong(value) {
-        this.writeByte(value >> 56);
-        this.writeByte(value >> 48);
-        this.writeByte(value >> 40);
-        this.writeByte(value >> 32);
-        this.writeByte(value >> 24);
-        this.writeByte(value >> 16);
-        this.writeByte(value >> 8);
-        this.writeByte(value);
+        var view = new DataView(new ArrayBuffer(8));
+        view.setBigInt64(0, value);
+        for (var i = 0; i < 8; i++)
+            this.writeByte(view.getUint8(i));
     }
     writeFloat(value) {
-        this.writeInt(this.floatToInt(value));
+        var view = new DataView(new ArrayBuffer(4));
+        view.setFloat32(0, value);
+        for (var i = 0; i < 4; i++)
+            this.writeByte(view.getUint8(i));
     }
     writeDouble(value) {
-        this.writeLong(this.doubleToLong(value));
+        var view = new DataView(new ArrayBuffer(8));
+        view.setFloat64(0, value);
+        for (var i = 0; i < 8; i++)
+            this.writeByte(view.getUint8(i));
     }
     writeBytes(value) {
         for (var c of value)
@@ -47,6 +60,60 @@ class DataOutputStream {
     writeChars(value) {
         for (var c of value)
             this.writeChar(c);
+    }
+    writeUTF(value) {
+        var len = value.length;
+        var i = 0;
+        var pos = 0;
+        var lengthWritten = false;
+        var buf = []; buf.length = 512;
+        
+        do {
+            while (i < len && pos < buf.length - 3) {
+                var c = value.charAt(i++);
+                var cint = c.charCodeAt(0);
+                if (c >= '\u0001' && c <= '\u007f')
+                    buf[pos++] = cint;
+                else if (c == '\u0000' || (c >= '\u0080' && c <= '\u07ff')) {
+                    buf[pos++] = (0xc0 | (0x1f & (cint >> 6)));
+                    buf[pos++] = (0x80 | (0x3f & cint));
+                } else {
+                    // JSL says the first byte should be or'd with 0xc0, but
+                    // that is a typo.  Unicode says 0xe0, and that is what is
+                    // consistent with DataInputStream.
+                    buf[pos++] = (0xe0 | (0x0f & (cint >> 12)));
+                    buf[pos++] = (0x80 | (0x3f & (cint >> 6)));
+                    buf[pos++] = (0x80 | (0x3f & cint));
+                }
+            }
+            if (!lengthWritten) {
+                if (i == len)
+                    this.writeShort(pos);
+                else
+                    this.writeShort(this.getUTFlength(value, i, pos));
+                lengthWritten = true;
+            }
+            this.writeBuffer(buf, 0, pos);
+            pos = 0;
+        } while (i < len);
+    }
+    getUTFlength(value, start, sum) {
+        var len = value.length;
+        
+        for (var i = start; i < len && sum <= 65535; ++i) {
+            var c = value.charAt(i);
+            if (c >= '\u0001' && c <= '\u007f')
+                sum += 1;
+            else if (c == '\u0000' || (c >= '\u0080' && c <= '\u07ff'))
+                sum += 2;
+            else
+                sum += 3;
+        }
+        
+        if (sum > 65535)
+            throw new Error("UTFDataFormatException");
+        
+        return sum;
     }
     length() {
         return this.bytes.length;
